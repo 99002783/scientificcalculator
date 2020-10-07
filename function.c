2ddfaa9984408c093ee23e902632875741333049
99002783 <72496295+99002783@users.noreply.github.com>
2020-10-07 16:37:27 +0530

    Update function.c

--- function.c
+++ function.c
@@ -1,18 +1,94 @@
 #include<function.h>
 
-double FindSimple(double principle,double rate,double time)
+void add()
 {
-	
-	double si=principle*rate*time;
-	
-	si=si/100;
-	
-	return si;
+    int n, total=0, i, number;
+    printf("\nEnter the number of elements you want to add:");
+    scanf("%d",&n);
+    printf("Please enter %d numbers one by one: \n",n);
+   for(i=0;i<=n;i++)
+   {
+        scanf("%d",&number);
+        total=total+number;
+        
+    }
+    printf("Sum of %d numbers = %d \n",n,total);
+}
+ 
+void sub()
+{ 
+    int a, b,c; 
+    printf("\nPlease enter first number  : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number : "); 
+    scanf("%d", &b); 
+    c = a - b; 
+    printf("\n%d - %d = %d\n", a, b, c); 
+}
+ 
+void mul()
+{
+    int a, b, mul=0; 
+    printf("\nPlease enter first numb   : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number: "); 
+    scanf("%d", &b);
+    mul=a*b;
+    printf("\nMultiplication of entered numbers = %d\n",mul);
+}
+ 
+void div()
+{
+    int a, b, d=0; 
+    printf("\nPlease enter first number  : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number : "); 
+    scanf("%d", &b);
+    d=a/b;
+    printf("\nDivision of entered numbers=%d\n",d);
+}
+ 
+void modulus()
+{
+    int a, b, d=0; 
+    printf("\nPlease enter first number   : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number  : "); 
+    scanf("%d", &b);
+    d=a%b;
+    printf("\nModulus of entered numbers = %d\n",d);
+}
+ 
+void power()
+{
+    double a,num, p;
+    printf("number: ");
+    scanf("%lf",&a);
+ 
+    printf("power : ");
+    scanf("%lf",&num);
+ 
+    p=pow(a,num);
+ 
+    printf("\n%lf to the power %lf = %lf \n",a,num,p);
+}
+ 
+int factorial()
+{
+    int i,fact=1,num;
+ 
+    printf("\nEnter a number to find factorial : ");
+    scanf("%d",&num);
+ 
+    if (num<0)
+    {
+        return 1;
+    }               
+ 
+    for(i=1;i<=num;i++)
+    fact=fact*i;
+    printf("\n");
+    printf("Factorial of entered number %d is:%d\n",num,fact);
+    return 0;
 }
 
-double FindCompound(double P, double R, double T) 
-{
-   double CI= P*(pow(1+(R/100), T));
-   
-   return CI;
-}
