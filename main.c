c51f099d405acf202131bbddba0832aa0094c853
99002783 <72496295+99002783@users.noreply.github.com>
2020-10-07 16:38:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,86 +2,35 @@
 
 int main()
 {
-	
-double principle,rate,time,simple_intrest;
-
-printf("enter principle");
-printf("\n");
-
-scanf("%lf",&principle);
-
-printf("enter rate of intrest");
-printf("\n");
-
-scanf("%lf",&rate);
-
-printf("enter time in months");
-printf("\n");
-
-scanf("%lf",&time);
-
-//principle value greater than zero
-
-if(principle<=0)
-{
-	printf("enter enter the value greater than zero");
-	
-	exit(0);
-}
-
-//rate value greater than zero
-
-if(rate<=0)
-{
-	printf("enter enter the value greater than zero");
-	
-	exit(0);
-}
-
-//time value greater than zero
-
-if(time<=0)
-{
-	printf("enter enter the value greater than zero");
-	
-	exit(0);
-}
-
-printf("enter 1 for simple intrest");
-
-printf("\n");
-
-printf("enter 2 for compound intrest");
-
-printf("\n");
-
-int enter;
-
-scanf("%d",&enter);
-
-if(enter==1)
-{
-	
-	double SI=FindSimple(principle,rate,time);
-	
-	printf("%lf",SI);
-	
-}
-
-if(enter==2)
-{
-
-double CI=FindCompound(principle,rate,time);
-	
-	printf("%lf",CI);
-}
-
-if(enter>2)
-{
-	printf("please choose the correct value try again");
-	
-	exit(0);
-	
-}
-return 0;
-}
+    int key;
+    printf("[1] Add\t[2] Subtract\t[3] Multiply\t[4] Divide\t[5]modulus\t[6]factorial\t[7]power\n");
+    scanf("%d", &key);
+ 
+        switch(key)
+        {
+            case '1': add();
+                      break;
+ 
+            case '2': sub();
+                      break;
+ 
+            case '3': mul();
+                      break;
+ 
+            case '4': div();
+                      break;
+ 
+            case '5': modulus();
+                      break;
+ 
+            case '6': factorial();
+                      break;
+ 
+            case '7': power();
+                      break;
+            default:
+             printf("\n**********You have entered unavailable option");
+             printf("***********\n");
+ 
+        }
+    }
